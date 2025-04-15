d87e24a32635bf28afabb4b49e31f17eeed24d8c
Benny Chopra <bchopra@bsd220.org>
2025-04-15 10:02:37 -0500

    Initial Commit

---
+++ APIcalling/APIcallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIcallingApp.swift
+//  APIcalling
+//
+//  Created by Benny Chopra on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct APIcallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIcalling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APIcalling
+//
+//  Created by Benny Chopra on 4/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
